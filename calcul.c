3bd3f43b6518a4344aee3a0b46729d323f536a81
AdamDKR <230014@umons.ac.be>
2025-11-26 18:45:04 +0100

    Update calcul.c
    normalement le only-longest fonctionne mais ne compte pas le nombre de chiffre différent

--- calcul.c
+++ calcul.c
@@ -1,88 +1,172 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
+#include "led.h"
 #include "calcul.h"
+#include <string.h>
 
-//Solution pour la suite de Conway pour un certain nombre d'itérations en partant d'une valeur
-char *solve(const char *value, const int iterations) {
+char *run(FILE *input_file, FILE *output_file, int only_longest) {
 
-	size_t len_value = strlen(value);
-	char *current_value = malloc(len_value + 1);
-	if (current_value == NULL) {
-		printf("Error: malloc failed\n");
-		return NULL;
-	}
+        char buffer[4000];
+        int nbr_digits = 0;
+        char *cont = NULL;
 
-	char *new_value = NULL;
 
-	strcpy(current_value, value);
+        while (fgets(buffer, sizeof(buffer), input_file) != NULL) {
+                buffer[strcspn(buffer, "\n")] = 0;
 
-	size_t len_current_value = len_value;
+                char *separator = strchr(buffer, ' ');
+                if (separator != NULL) {
+                        *separator = '\0';
 
-	for (size_t i = 0; i < iterations; i++) {
+                        char *value = buffer;
+                        char *iter_str = separator + 1;
 
-		size_t j = 0;
-		size_t len = 0;
+                        if (strlen(value) == 0) {
+                                fprintf(stderr, "Error: The initial value is empty.\n");
+                                continue;
+                        }
 
-		new_value = realloc(new_value, 2 * len_current_value + 1);
-        	if (new_value == NULL) {
-            		printf("Error: realloc failed\n");
-            		free(current_value);
-            		return NULL;
-        	}
+                        if (strlen(iter_str) == 0) {
+                                fprintf(stderr, "Error: The number of iterations is empty.\n");
+                                continue;
+                        }
 
-		while (j < len_current_value) {
+                        int iterations = atoi(iter_str);
 
-			size_t orig_j = j;
-			char c = current_value[j];
-			j++;
+                        if (iterations < 0) {
+                                fprintf(stderr, "Error: The number of iterations is invalid.\n");
+                                continue;
+                        }
 
-			while (j < len_current_value && current_value[j] == c) {
+                        char *result = solve(value, iterations);
+                        if ( only_longest == 0 ) {
+                                if (result != NULL) {
+                                        fprintf(output_file, "%s\n", result);
+                                        free(result);
+                                } else {
+                                        fprintf(stderr, "Error: The calculation did not work.");
+                                }
+                        } else {
 
-				j++;
-
-			}
-
-			size_t count = j - orig_j;
-
-			len += sprintf(new_value + len, "%zu%c", count, c);
-
-		}
-
-		new_value[len] = '\0';
-
-		char *tmp = current_value;
-		current_value = new_value;
-		new_value = tmp;
-
-		len_current_value = len;
-
-	}
-
-	free(new_value);
-	return current_value;
-
+                                if ( strlen(result) == nbr_digits ) {
+                                        char *tempo;
+                                        tempo=realloc(cont,strlen(cont)+strlen(value)+strlen(iter_str)+3 );
+                                        if ( tempo==NULL ){
+                                                fprintf(stderr,"Erreur mémoire");
+                                                return NULL;
+                                        }
+                                        cont=tempo;
+                                        sprintf(cont+strlen(cont), "\n%s %s",value,iter_str);
+                                } else if ( strlen(result) > nbr_digits ) {
+                                        free(cont);
+                                        cont = NULL;
+                                        cont=realloc(cont,strlen(value)+strlen(iter_str)+2);
+                                        if ( cont==NULL ){
+                                                fprintf(stderr,"Erreur mémoire");
+                                                return NULL;
+                                        }
+                                        sprintf(cont, "%s %s",value,iter_str);
+                                        nbr_digits=strlen(result);
+                                }
+                                free(result);
+                                //printf("%d",nbr_digits);
+                        }
+                } else {
+                        fprintf(stderr, "Error: Invalid format\n");
+                        continue;
+                }
+        }
+        return cont;
 }
 
-//Cette fonction compte le nombre de chiffre différents lorsqu'on lui donne un "string"
-int count_digits(const char *number) {
 
-	int digits[10] = {0};
-	int count = 0;
+int main(int argc, char *argv[]) {
 
-	for (size_t i = 0; number[i] != '\0'; i++) {
+        char *input_path = NULL;
+        char *output_path = NULL;
+        int only_longest = 0;
+        int debug = 0;
 
-		int digit = number[i] - '0';
+        for (int i = 1; i < argc; i++) {
+                if (strcmp(argv[i], "--input") == 0) {
+                        if (i + 1 < argc) {
+                                input_path = argv[++i];
+                        } else {
+                                fprintf(stderr, "Error: The --input option expects a file path.\n");
+                                return 1;
+                        }
+                } else if (strcmp(argv[i], "--output") == 0) {
+                        if (i + 1 < argc) {
+                                output_path = argv[++i];
+                        } else {
+                                fprintf(stderr, "Error: The --output option expects a file path.\n");
+                                return 1;
+                        }
+                } else if (strcmp(argv[i], "--only-longest") == 0) {
 
-		if (!digits[digit]) {
+                        only_longest = 1;
 
-			digits[digit] = 1;
-			count++;
+                } else if (strcmp(argv[i], "--debug") == 0) {
+                        if (i + 1 < argc) {
+                                debug = atoi(argv[++i]);
+                        } else {
+                                fprintf(stderr, "Error: The --debug option expects a number.\n");
+                                return 1;
+                        }
+                }
+        }
 
-		}
+        FILE *input_file = stdin;
+        if (input_path != NULL) {
+                input_file = fopen(input_path, "r");
+                if (input_file == NULL) {
+                        fprintf(stderr, "Error: Unable to open the input file %s.\n", input_path);
+                        return 1;
+                }
+        }
 
-	}
+        FILE *output_file = stdout;
+        if (output_path != NULL) {
+                output_file = fopen(output_path, "w");
+                if (output_file == NULL) {
+                        fprintf(stderr, "Error: Unable to open the output file %s.\n", output_path);
+                        if (input_path != NULL) {
+                                fclose(input_file);
+                        }
+                        return 1;
+                }
+        }
 
-	return count;
+        char *second_input=run(input_file, output_file,only_longest);
+        if (only_longest == 1) {
+                if (second_input != NULL) {
+                        FILE *flux_memoire = fmemopen(second_input, strlen(second_input), "r");
+
+                        if (flux_memoire != NULL) {
+                                char *second_output = run(flux_memoire, output_file, 0);
+                                free(second_output);
+                                fclose(flux_memoire);
+
+                        } else {
+                                fprintf(stderr, "Erreur: Echec de fmemopen\n");
+                        }
+                }
+        }
+
+        free(second_input);
+        if (input_path != NULL) {
+
+                fclose(input_file);
+
+        }
+
+        if (output_path != NULL) {
+
+                fclose(output_file);
+
+        }
+
+        return 0;
 
 }
